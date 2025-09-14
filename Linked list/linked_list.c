572d66065b64c00eb1bcd8dc4b54088aaa7bc45f
MeetUnadakat07 <meetunadakat06@gmail.com>
2025-09-15 00:12:06 +0530

    update linked list

--- Linked list/linked_list.c
+++ Linked list/linked_list.c
@@ -30,6 +30,7 @@ void printData(struct node *head) {
         printf("%d ", ptr -> data);
         ptr = ptr -> link;
     }
+    printf("\n");
 }
 
 void insertAtEnd(struct node *head, int data) {
@@ -55,6 +56,78 @@ struct node *insertAtBegining(struct node *head, int data) {
     return temp;
 }
 
+void *insertAtPosition(struct node *head, int data, int position) {
+    struct node *ptr = head;
+    struct node *temp = malloc(sizeof(struct node));
+    temp -> data = data;
+    temp -> link = NULL;
+
+    if(position == 1) {
+        temp -> link = head;
+        head = temp;
+        return head;
+    } else if(head == NULL) {
+        insertAtBegining(head, data);
+    } else {
+        while(position != 2) {
+            ptr = ptr -> link;
+            position--;
+        }
+        temp -> link = ptr -> link;
+        ptr -> link = temp;
+    }
+}
+
+struct node *deleteAtBegining(struct node *head) {
+    if(head == NULL) {
+        printf("List is already empty.\n");
+    } else {
+        struct node *temp = head;
+        head = head -> link;
+        free(temp);
+        temp = NULL;
+    }
+    return head;
+}
+
+struct node *deleteAtEnd(struct node *head) {
+    if(head == NULL) {
+        printf("List is already empty.\n");
+        return NULL;
+    } else if(head -> link == NULL) {
+        free(head);
+        return NULL;
+    } else {
+        struct node *temp = head;
+        while(temp -> link -> link != NULL) {
+            temp = temp -> link;
+        }
+        free(temp -> link);
+        temp -> link = NULL;
+        return head;
+    }
+}
+
+struct node *deleteAtPosition(struct node *head, int position) {
+    struct node *prev = head;
+    struct node *curr = head;
+    if(head == NULL) {
+        printf("List is already empty.\n");
+        return NULL;
+    } else if(position == 1) {
+        return deleteAtBegining(head);
+    } else {
+        while(position != 1) {
+            prev = curr;
+            curr = curr -> link;
+            position--;
+        }
+        prev -> link = curr -> link;
+        free(curr);
+        return head;
+    }
+}
+
 int main() {
     struct node *head = malloc(sizeof(struct node));
     head -> data = 45;
@@ -66,10 +139,20 @@ int main() {
     printf("The elements of the list are: ");
     printData(head);
 
-    printf("\n");
-
     head = insertAtBegining(head, 56);
     printData(head);
     countOfNodes(head);
+
+    head = insertAtPosition(head, 48, 1);
+    printData(head);
+
+    head = deleteAtBegining(head);
+    printData(head);
+
+    head = deleteAtEnd(head);
+    printData(head);
+
+    head = deleteAtPosition(head, 2);
+    printData(head);
     return 0;
 }
